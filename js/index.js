9c1f88f1fac1b5c2cad04409d324127b0f5f9f5b
namyunje <d.namyunje@gmail.com>
2020-07-16 22:13:39 +0900

    index.html's structure change

--- js/index.js
+++ js/index.js
@@ -1,21 +1,28 @@
 const searchButton = document.querySelector(".search_span");
 const searchInput = document.querySelector(".input_search");
 const testNavbar = document.querySelector(".navbar_sub");
-
-console.log(testNavbar);
+const navbarNav01 = document.querySelector(".navbar_nav01");
+const navbarSub = document.querySelector(".navbar_nav_sub_wrap");
+const navbarSubInner = document.querySelector(".navbar_nav_sub_inner");
 
 searchButton.addEventListener("click", () => {
-  const navbarUtil = document.querySelector(".navbar_util");
-
   if (searchInput.clientWidth < 130) {
     searchInput.style.display = "block";
     let increaseSearchInput = setInterval(() => {
       if (searchInput.clientWidth >= 130) {
-        // navbarUtil.style.width = 680 + "px";
-        // navbarUtil.style.right = 45 + "px";
         clearInterval(increaseSearchInput);
       }
       searchInput.style.width = searchInput.clientWidth + 1 + "px";
     }, 10);
   }
+});
+
+navbarNav01.addEventListener("mouseover", () => {
+  const navbarSub01 = document.querySelector("#navbar_sub01");
+
+  navbarSub01.style.display = "block";
+});
+
+navbarSub.addEventListener("mouseleave", () => {
+  navbarSubInner.style.display = "none";
 });
